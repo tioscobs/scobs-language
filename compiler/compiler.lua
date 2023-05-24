d32fbd1be31ccfc0b4a6631a4fcdb1cd10f17b5e
Scubi <supermoondoge@gmail.com>
2023-05-23 21:36:20 -0300

    compiler update

--- compiler/compiler.lua
+++ compiler/compiler.lua
@@ -4,12 +4,27 @@ local fileLines = {}
 
 function start_alg()
     if #fileLines > 0 then
-        
+        for i, v in pairs(fileLines) do
+            local raw_text = string.gsub(v, "%s", "")
+            if string.match(raw_text, "[%a]+") then
+                --print(i)
+            end
+        end
+    end
+end
+
+function check_comments()
+    if #fileLines > 0 then
+        for i, v in pairs(fileLines) do
+            if string.find(v, "@@") then
+                print(i)
+            end
+        end
     end
 end
 
 function open_file()
-    local file_dir = "D:\\scobs-language\\compiler\\test.scobs" --io.read()
+    local file_dir = io.read()
     local bool, file = pcall(io.open, file_dir)
     if file ~= nil then
         for line in file:lines() do
@@ -18,7 +33,7 @@ function open_file()
         start_alg()
         file:close()
     else
-        print("O Caminho especificado nao existe.")
+        print("O caminho especificado nao existe.")
     end
 end
 
